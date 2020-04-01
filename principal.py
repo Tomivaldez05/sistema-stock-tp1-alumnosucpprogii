7a144e0be85e44d5921ced9f586604a47d4d7900
leonelcajal <leocajald@gmail.com>
2020-04-01 16:34:43 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -3,6 +3,8 @@ window = Tk()
 
 window.title("Programación II - Sistema Stock")
 window.title("Programación II - Sistema")
+window.geometry("400x400")
+Label(window,text="sistema stock").grid(row=0,column=0)
 
 
 window.mainloop()
