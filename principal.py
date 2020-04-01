d24f527ad9f9264a2d20701c382374a46fadfb24
mauro-15-02 <63015171+mauro-15-02@users.noreply.github.com>
2020-04-01 16:13:28 -0300

    comit_1

--- principal.py
+++ principal.py
@@ -2,5 +2,7 @@ from tkinter import *
 window = Tk()
 
 window.title("Programación II - Sistema Stock")
-
+c= Canvas(window,width=200, height=200)
+c.create_rectangle(10,10,160,160)
+c.place(x=0,y=0)
 window.mainloop()
