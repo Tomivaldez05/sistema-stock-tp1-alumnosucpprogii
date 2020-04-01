5a74e7934f755f383eded3b07874b4c45e1efcd6
Ing. Federico Machuca <nesfdm@gmail.com>
2020-04-01 16:36:47 -0300

    agrego label

--- principal.py
+++ principal.py
@@ -5,6 +5,6 @@ window.title("Programación II - Sistema Stock")
 window.title("Programación II - Sistema")
 window.geometry("400x400")
 Label(window,text="sistema stock").grid(row=0,column=0)
-
+Label(window,text="sistema stock").grid(row=0,column=0)
 
 window.mainloop()
